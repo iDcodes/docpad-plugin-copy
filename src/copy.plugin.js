34f473bb4aed6d95d999355290186970b1e23069
Arbin Reyes <arbin.reyes@nri-anz.com>
2025-11-06 09:38:54 +0800

    refactore copy.plugin.js

--- src/copy.plugin.js
+++ src/copy.plugin.js
@@ -1,79 +1,89 @@
 // Modernized ES6+ version of docpad-plugin-copy
-
-const eachr = require("eachr");
-const pathUtil = require("path");
-const { TaskGroup } = require("taskgroup");
-const safeps = require("safeps");
-
 module.exports = function (BasePlugin) {
-  class Copy extends BasePlugin {
-    constructor(...args) {
-      super(...args);
-      this.name = "copy";
-    }
+    class Copy extends BasePlugin {
+        constructor(...args) {
+            super(...args);
+            this.name = "copy";
+        }
 
-    async writeAfter(opts, next) {
-      const docpad = this.docpad;
-      const config = this.getConfig();
+        writeAfter(opts, next) {
+            const eachr = require('eachr');
+            const pathUtil = require('path');
+            const docpad = this.docpad;
+            const config = this.getConfig();
+            const docpadConfig = this.docpad.getConfig();
+            const outPath = pathUtil.normalize(docpad.getPath('out'));
+            const srcPath = pathUtil.normalize(docpad.getPath('source'));
 
-      const outPath = pathUtil.normalize(docpad.getPath('out'));
-      const srcPath = pathUtil.normalize(docpad.getPath('source'));
-
-      // Default config if none provided
-      if (Object.keys(config).length === 0) {
-        config.default = { src: "raw" };
-      }
-
-      const tasks = new TaskGroup({ concurrency: 1 })
-        .done((err) => {
-          if (!err) docpad.log("info", "Copying completed successfully");
-          else docpad.log("error", "Copying error: " + err);
-
-          if (typeof next === "function") next();
-        });
-
-      eachr(config, (target, key) => {
-        tasks.addTask((complete) => {
-          const src = pathUtil.join(srcPath, target.src);
-          let out = outPath;
-
-          if (target.out) {
-            out = pathUtil.join(outPath, target.out);
-          }
-
-          const options =
-            target.options && typeof target.options === "object"
-              ? target.options
-              : {};
-
-          docpad.log("info", `Copying ${key} → out: ${out}, src: ${src}`);
-
-          const WINDOWS = process.platform === "win32";
-          const OSX = process.platform === "darwin";
-          const CYGWIN = /cygwin/i.test(process.env.PATH);
-          const XCOPY = WINDOWS && !CYGWIN;
-
-          const command = XCOPY
-            ? ["xcopy", "/eDy", src + "\\*", out + "\\"]
-            : OSX
-            ? ["rsync", "-a", src + "/", out + "/"]
-            : ["cp", "-Ruf", src + "/.", out];
-
-          safeps.spawn(
-            command,
-            { output: false },
-            (err) => {
-              if (err) return complete(err);
-              docpad.log("debug", `Done copying ${key}`);
-              complete();
+            // Set default config if empty
+            if (Object.keys(config).length === 0) {
+                config.default = {
+                    src: 'raw'
+                };
             }
-          );
-        });
-      });
 
-      tasks.run();
+            const TaskGroup = require('taskgroup').TaskGroup;
+            const tasks = new TaskGroup({
+                concurrency: 1
+            }).done((err, results) => {
+                if (!err) {
+                    docpad.log('info', 'Copying completed successfully');
+                } else {
+                    docpad.log('error', `Copying error ${err}`);
+                }
+
+                if (typeof next === 'function') {
+                    next();
+                }
+            });
+
+            eachr(config, (target, key) => {
+                tasks.addTask((complete) => {
+                    const src = pathUtil.join(srcPath, target.src);
+                    let out = outPath;
+
+                    if (target.out != null) {
+                        out = pathUtil.join(outPath, target.out);
+                    }
+
+                    const options = (target.options != null && typeof target.options === 'object')
+                        ? target.options
+                        : {};
+
+                    docpad.log('info', `Copying ${key} out: ${out}, src: ${src}`);
+
+                    const WINDOWS = /win32/.test(process.platform);
+                    const OSX = /darwin/.test(process.platform);
+                    const CYGWIN = /cygwin/.test(process.env.PATH);
+                    const XCOPY = WINDOWS && !CYGWIN;
+
+                    let command;
+                    if (XCOPY) {
+                        command = ['xcopy', '/eDy', `${src}\\*`, `${out}\\`];
+                    } else if (OSX) {
+                        command = ['rsync', '-a', `${src}/`, `${out}/`];
+                    } else {
+                        command = ['cp', '-Ruf', `${src}/.`, out];
+                    }
+
+                    const safeps = require('safeps');
+
+                    return safeps.spawn(command, {
+                        output: false
+                    }, (err) => {
+                        if (err) {
+                            return complete(err);
+                        }
+
+                        docpad.log('debug', `Done copying ${key}`);
+                        return complete();
+                    });
+                });
+            });
+
+            return tasks.run();
+        }
     }
-  }
 
-  return Copy;
+    return Copy;
 };
